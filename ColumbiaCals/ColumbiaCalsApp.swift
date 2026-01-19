4ced8d258543bb195b47959ee6df05fd5e6a1ce5
michelliez <michellezhou7788@gmail.com>
2026-01-19 18:11:01 -0500

    Initial Commit

---
+++ ColumbiaCals/ColumbiaCalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColumbiaCalsApp.swift
+//  ColumbiaCals
+//
+//  Created by Michelle Zhou on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct ColumbiaCalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
